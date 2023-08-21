d87b43143c6dd232f8e3a71cf25ab4a2736c7afe
jarathomas <jaratthomas@gmail.com>
2023-08-21 16:44:12 -0400

    adding description for pypi and reading package info from __version__.py

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 import os
+from pathlib import Path
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(here, "pycrossva", "__version__.py"),
+          mode="r", encoding="utf-8") as f:
+    exec(f.read(), about)
 
-setup(name="pycrossva",
-      version="0.95",
-      description="prepare data from WHO and PHRMC instruments for verbal autopsy algorithms",
-      url="https://github.com/verbal-autopsy-software/pyCrossVA",
-      license="GNU General Public License v3.0",
+this_directory = Path(__file__).parent
+readme = (this_directory / "Readme.rst").read_text()
+
+setup(name=about["__title__"],
+      version=about["__version__"],
+      description=about["__description__"],
+      long_description=readme,
+      long_description_content_type="text/x-rst",
+      url=about["__url__"],
+      author=about["__author__"],
+      maintainer=about["__maintainer__"],
+      maintainer_email=about["__maintainer_email__"],
+      license=about["__license__"],
       packages=[
           "pycrossva",
           "pycrossva.resources.mapping_configuration_files",
